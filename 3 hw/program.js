c2a5db161bcff4ec5da54ec474cdb6b410581bfc
Zem4ik <vladbullzv@gmail.com>
2017-06-20 17:07:49 +0300

    hw3 fix

--- 3 hw/program.js
+++ 3 hw/program.js
@@ -12,14 +12,14 @@ lastStrings.push("@a((a+0')*(a+0')=(a*a)+(0''*a)+0')->");
 lastStrings.push("((a+0')*(a+0')=(a*a)+(0''*a)+0')");
 lastStrings.push("((a+0')*(a+0')=(a*a)+(0''*a)+0')");
 
-let count = fs.readFileSync('input.txt', 'utf8');
+let count = fs.readFileSync('number.txt', 'utf8');
 let formattedNumber = "0";
 for (let i = 0; i < count; ++i) {
     formattedNumber += '\'';
 }
 
 let proof = firstString.replace(/a/g, formattedNumber) + '\n';
-proof += count + '\n';
+proof += fs.readFileSync('input.txt', 'utf8') + '\n';
 proof += lastStrings[0] ;
 proof += lastStrings[1].replace(/a/g, formattedNumber) + '\n';
 proof += lastStrings[2].replace(/a/g, formattedNumber);
